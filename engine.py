08ebf3e865748b269129832ba8c9695351cc52a1
h4xxel <h4xxel@h4xx.org>
2011-03-25 12:56:55 +0100

    animation

--- engine.py
+++ engine.py
@@ -1,4 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+#!/usr/bin/env python
 import pyglet
+from pyglet.window import key
+
+class Sprite(pyglet.sprite.Sprite):
+	animation=None
+	x=0; y=0
+	def __init__(self, img, x, y, width, height):
+		frames=[]
+		image=pyglet.resource.image(img)
+		for i in range(0, image.width, width):
+			frames.append([image.get_region(i, 0, width, height), 0.1])
+		self.animation=Animation(frames)
+		self.x=x; self.y=y
+		
+	def draw(self):
+		self.animation.blit(self.x, self.y)
+	
+	def animate(self):
+		self.animation.progress()
+
+class Animation():
+	frames=[[]]
+	active_frame=0
+	def __init__(self, frames):
+		self.frames=frames
+	
+	def progress(self):
+		self.active_frame+=1
+		if(self.active_frame>=len(self.frames)): self.active_frame=0
+			
+	def blit(self, x, y):
+		image=self.frames[self.active_frame][0]
+		image.blit(x, y)
+			
+class AnimationFrame():
+	image=None
+	duration=None
+	def __init__(self, image, duration):
+		self.image=image
+		self.duration=duration
+	def __getitem__(self, lol):
+		return [self.image, self.duration]
--- the_wave.py
+++ the_wave.py
@@ -2,7 +2,34 @@
 # -*- coding: UTF-8 -*-
 
 import pyglet
-from pyglet.window import key
-import engine
+from engine import *
 
-window=pyglet.window.Window()
+class Window(pyglet.window.Window):
+	def on_draw(self):
+		window.clear()
+		sprite.draw()
+		label.draw()
+		
+def update(dt):
+	if(keys[key.A]):
+		sprite.x-=5
+		sprite.animate()
+	if(keys[key.D]):
+		sprite.x+=5
+		sprite.animate()
+	if(keys[key.W]):
+		sprite.y+=5
+		sprite.animate()
+	if(keys[key.S]):
+		sprite.y-=5
+		sprite.animate()
+
+label=pyglet.text.Label("lol", x=20, y=50)
+
+
+sprite=Sprite(img="test.png", x=100, y=100, width=64, height=64)
+keys=key.KeyStateHandler()
+window=Window()
+window.push_handlers(keys)
+pyglet.clock.schedule(update)
+pyglet.app.run()
