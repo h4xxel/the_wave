3992abc2e8a05084c9b0965a39d89ce56f7a2461
h4xxel <h4xxel@h4xx.org>
2011-03-29 23:27:02 +0200

    improved jumping ..or maybe not

--- the_wave.py
+++ the_wave.py
@@ -14,19 +14,22 @@ class Window(pyglet.window.Window):
 def update(dt):
 	sprite.update(ground.y+ground.height)
 	background.update()
-	if(keys[key.A]):
-		sprite.x-=5
-		sprite.animate()
-	if(keys[key.D]):
-		sprite.x+=5
-		sprite.animate()
-	if(keys[key.SPACE] and sprite.y<=ground.y+ground.height):
-		sprite.vspeed+=10
-		sprite.y+=1
-		sprite.animate()
+	if(sprite.y<=ground.y+ground.height):
+		if(keys[key.A]):
+			sprite.hspeed=-5
+			sprite.animate()
+		elif(keys[key.D]):
+			sprite.hspeed=5
+			sprite.animate()
+		else:
+			sprite.hspeed=0
+		if(keys[key.SPACE]):
+			sprite.vspeed+=5
+			sprite.y+=1
+			sprite.animate()
 
 ground=150
-sprite=Sprite(img="test.png", x=100, y=500, width=64, height=64, gravity=-0.2)
+sprite=Sprite(img="snubbe.png", x=100, y=500, width=32, height=32, gravity=-0.2)
 background=Sprite(img="background.png", x=0, y=0, width=1600, height=600, hspeed=-2)
 ground=Sprite(img="ground.png", x=0, y=0, width=1600, height=150, hspeed=-2)
 keys=key.KeyStateHandler()
