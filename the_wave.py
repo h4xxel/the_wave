dbf1c9da89e970f0c27885641598a56ffa64a9ca
h4xxel <h4xxel@h4xx.org>
2011-04-04 15:02:40 +0200

    walking on terrain

--- the_wave.py
+++ the_wave.py
@@ -9,21 +9,23 @@ class Window(pyglet.window.Window):
 	def on_draw(self):
 		window.clear()
 		background.draw()
-		#ground.draw()
 		pyglet.graphics.draw(len(terrain)/2, pyglet.gl.GL_TRIANGLE_STRIP, ("v2i", tuple(terrain)))
 		sprite.draw()
 		
 def update(dt):
 	background.x+=background.hspeed
 	sprite.x+=sprite.hspeed
-	if sprite.y<=ground.y+ground.height:
+	
+	if sprite.y<=get_terrain_y(sprite.x):
 		sprite.y=ground.y+ground.height
 		sprite.vspeed=0
+		sprite.y=get_terrain_y(sprite.x+(sprite.width/2))
 	else:
 		sprite.vspeed+=sprite.gravity
 		sprite.y+=sprite.vspeed
 	
-	if(sprite.y<=ground.y+ground.height):
+	if(sprite.y-5<=get_terrain_y(sprite.x)):
+		sprite.y=get_terrain_y(sprite.x+(sprite.width/2))
 		if(keys[key.A]):
 			sprite.hspeed=-3
 			sprite.animate()
@@ -34,7 +36,7 @@ def update(dt):
 			sprite.hspeed=0
 		if(keys[key.SPACE]):
 			sprite.vspeed+=7
-			sprite.y+=1
+			sprite.y+=10
 			sprite.animate()
 			
 def init_terrain():
@@ -42,6 +44,16 @@ def init_terrain():
 	for i in range(100, 800+100, 100):
 		terrain.extend([i, 0, i+100,random.randint(50, 100)])
 	return terrain
+	
+def get_terrain_y(x):
+	y1=terrain[4*(x//100)+1]
+	y2=terrain[4*(x//100)+5]
+	dx=float(x-100*(x//100))
+	#~ if(y1>y2):
+		#~ return int(y2+float(abs(y2-y1))/(1+(dx/100)))
+	#~ else:
+		#~ return int(y2-float(abs(y2-y1))/(1+(dx/100)))
+	return y1+int(float(y2-y1)*(dx/100))
 
 terrain=init_terrain()
 sprite=Sprite(img="snubbe.png", x=100, y=500, width=32, height=32, gravity=-0.2)
