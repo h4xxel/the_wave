c886b061e292bae96c329c19ee9b84da871832bf
h4xxel <h4xxel@h4xx.org>
2011-04-04 12:32:31 +0200

    started work on random terrain

--- engine.py
+++ engine.py
@@ -1,57 +1,64 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-#!/usr/bin/env python
 import pyglet
 from pyglet.window import key
 
+
+#use our own sprite class, because pyglet's sprite class is buggy with animations
 class Sprite(pyglet.sprite.Sprite):
 	animation=None
 	x=0; y=0
 	hspeed=0; vspeed=0
 	gravity=0
-	width=0; height=0
 	def __init__(self, img, x, y, width, height, hspeed=0, vspeed=0, gravity=0):
 		frames=[]
 		image=pyglet.resource.image(img)
 		for i in range(0, image.width, width):
-			frames.append([image.get_region(i, 0, width, height), 0.1])
+			frames.append([image.get_region(i, 0, width, height), 0.3])
 		self.animation=Animation(frames)
 		self.x=x; self.y=y
 		self.hspeed=hspeed; self.vspeed=vspeed
 		self.gravity=gravity
-		self.width=width; self.height=height
 		
 	def draw(self):
 		self.animation.blit(self.x, self.y)
 	
 	def animate(self):
 		self.animation.progress()
+	
+	def get_width(self):
+		return self.animation.width
 		
-	def update(self, ground=-100):
-		self.x+=self.hspeed
-		if self.y<=ground:
-			self.y=ground
-			self.vspeed=0
-		else:
-			self.vspeed+=self.gravity
-			self.y+=self.vspeed
+	def get_height(self):
+		return self.animation.height
+		
+	width=property(get_width); height=property(get_height)
 		
 
 class Animation():
+	# [[image, speed], [image, speed]] ...
 	frames=[[]]
 	active_frame=0
 	def __init__(self, frames):
 		self.frames=frames
 	
 	def progress(self):
-		self.active_frame+=1
+		self.active_frame+=self.frames[int(self.active_frame)][1]
 		if(self.active_frame>=len(self.frames)): self.active_frame=0
 			
 	def blit(self, x, y):
-		image=self.frames[self.active_frame][0]
+		image=self.frames[int(self.active_frame)][0]
 		s=pyglet.sprite.Sprite(img=image, x=x, y=y)
 		s.draw()
+	
+	def get_width(self):
+		return self.frames[int(self.active_frame)][0].width
+		
+	def get_height(self):
+		return self.frames[int(self.active_frame)][0].height
+	
+	width=property(get_width); height=property(get_height)
 			
 class AnimationFrame():
 	image=None
