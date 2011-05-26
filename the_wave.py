66f2db384fe8c8ce643e3e679858f944153f7c38
h4xxel <h4xxel@h4xx.org>
2011-05-27 00:01:56 +0200

    mac icon, setup script and license

--- the_wave.py
+++ the_wave.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+#~ The Wave - Small game where the goal is to survive a catastrophy and help others along the way
+#~ Copyright (C) 2011  h4xxel, niz3, Cygnus4
+
+#~ This program is free software; you can redistribute it and/or
+#~ modify it under the terms of the GNU General Public License
+#~ as published by the Free Software Foundation; either version 2
+#~ of the License, or (at your option) any later version.
+
+#~ This program is distributed in the hope that it will be useful,
+#~ but WITHOUT ANY WARRANTY; without even the implied warranty of
+#~ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#~ GNU General Public License for more details.
+
+#~ You should have received a copy of the GNU General Public License
+#~ along with this program; if not, write to the Free Software
+#~ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 import pyglet
 from pyglet.gl import *
 from engine import *
